8ff5632406d180449dd7a8022a58db5fe0ee9e6e
MILDS2015 <milan.goel@yahoo.com>
2015-07-23 21:17:33 -0400

    Changes for Assignment 2 Best of Luckls -lrt

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,28 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+ invert <- NULL
+        
+        set <- function(y){
+                orig <<- y  
+                # ensures that the value is set lexically to the formal argument
+                invert <<- null                  
+                # ensures that the value is set lexically to the informal argument invert
+        }
+        
+        get <- function(){ orig }
+        
+        setinvert <- function(invert){
+                invert <<- invert     
+                # ensures that the value is set lexically to the informal argument invert
+        }
+        
+        getinvert <- function(){ invert}
+        
+        list(set = set, get = get,
+             setinvert = setinvert,
+             getinvert = getinvert)
+
 }
 
 
@@ -12,4 +34,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		 inverse <- x$getinvert()
+        
+        if(!is.null(inverse)){
+                message("getting cached data")
+                return(inverse)
+        }
+        
+        inverse <- solve(x$get())
+        
+        x$setinvert(inverse)
+        inverse
 }
